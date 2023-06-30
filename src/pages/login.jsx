7a70d76e3aa139883f3e8e0ab77ef71b67a56200
ramesestan <rameses.tan.21@gmail.com>
2023-06-30 14:33:34 +0800

    changed export to Login, removed comment

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,14 +1,9 @@
-
-
-
-
 let Login = () => {
-	return (
-		<div>
-			<h1>Home Page</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
 };
-//this is an edit
 
-export default Home;
+export default Login;

8e51a8e0c048fd9fae1e25960e4503d31c788263
ramesestan <rameses.tan.21@gmail.com>
2023-06-30 14:59:13 +0800

    simple login form

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,31 @@
+import React from "react";
+import styles from "./login.module.css";
 let Login = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className={styles["login-container"]}>
+      <div className={styles["left-side"]}>
+        <h1>Login Form</h1>
+        <form>
+          <div>
+            <label htmlFor="username">Username *</label>
+            <input type="text" id="username" required />
+          </div>
+          <div>
+            <label htmlFor="password">Password *</label>
+            <input type="password" id="password" required />
+          </div>
+          <div>
+            <a href="/forgot-password">Forgot Password</a>
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+        {/* Add your login form components here */}
+      </div>
+      <div className={styles["right-side"]}>
+        <img src="path_to_your_image" alt="Something" />
+      </div>
     </div>
   );
 };

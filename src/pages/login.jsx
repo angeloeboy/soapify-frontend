7774199cfeebf1c21becfbb31523b58ec1a6e1fa
ramesestan <rameses.tan.21@gmail.com>
2023-06-30 15:00:50 +0800

    gelo malibog

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,10 @@
 let Login = () => {
-	return (
-		<div>
-			<h1>This is the login page</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>This is the login page</h1>
+      <p>gelo malibog</p>
+    </div>
+  );
 };
 
 export default Login;

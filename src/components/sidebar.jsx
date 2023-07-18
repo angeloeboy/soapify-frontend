04631638e58177f27eef7ecd31cee7b4acfd0bcd
Angelo <angeloeboy10@gmail.com>
2023-07-18 08:40:54 +0800

    added lnks

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,83 +1,80 @@
+import Link from "next/link";
 import { useState } from "react";
 import styled from "styled-components";
 
 const SidebarContainer = styled.div`
-  position: fixed;
-  height: 100vh;
-  top: 0px;
-  left: 0px;
-  background-color: lightblue;
-  width: 256px;
+	position: fixed;
+	height: 100vh;
+	top: 0px;
+	left: 0px;
+	background-color: lightblue;
+	width: 256px;
 `;
 
 const Menu = styled.div`
-  padding: 8px;
-  cursor: pointer;
+	padding: 8px;
+	cursor: pointer;
 
-  &:hover {
-    background-color: lightgray;
-  }
+	&:hover {
+		background-color: lightgray;
+	}
 `;
 
 const SubMenu = styled.div`
-  margin-left: 16px;
+	margin-left: 16px;
 `;
 
 const Sidebar = () => {
-  const [isPosSubMenuOpen, setIsPosSubMenuOpen] = useState(false);
-  const [isInventorySubMenuOpen, setIsInventorySubMenuOpen] = useState(false);
-  const [isSettingsSubMenuOpen, setIsSettingsSubMenuOpen] = useState(false);
+	const [isPosSubMenuOpen, setIsPosSubMenuOpen] = useState(false);
+	const [isInventorySubMenuOpen, setIsInventorySubMenuOpen] = useState(false);
+	const [isSettingsSubMenuOpen, setIsSettingsSubMenuOpen] = useState(false);
 
-  const handlePosSubMenuToggle = () => {
-    setIsPosSubMenuOpen(!isPosSubMenuOpen);
-  };
+	const handlePosSubMenuToggle = () => {
+		setIsPosSubMenuOpen(!isPosSubMenuOpen);
+	};
 
-  const handleInventorySubMenuToggle = () => {
-    setIsInventorySubMenuOpen(!isInventorySubMenuOpen);
-  };
+	const handleInventorySubMenuToggle = () => {
+		setIsInventorySubMenuOpen(!isInventorySubMenuOpen);
+	};
 
-  const handleSettingsSubMenuToggle = () => {
-    setIsSettingsSubMenuOpen(!isSettingsSubMenuOpen);
-  };
+	const handleSettingsSubMenuToggle = () => {
+		setIsSettingsSubMenuOpen(!isSettingsSubMenuOpen);
+	};
 
-  return (
-    <SidebarContainer className="sidebar">
-      <h1>SOAPIFY</h1>
-      <Menu className="home-menu">Home</Menu>
-      <Menu className="pos-with with-submenu" onClick={handlePosSubMenuToggle}>
-        POS
-        {isPosSubMenuOpen && (
-          <SubMenu>
-            <div className="pos-sub-sales">Sales</div>
-          </SubMenu>
-        )}
-      </Menu>
-      <Menu
-        className="inventory-menu with-submenu"
-        onClick={handleInventorySubMenuToggle}
-      >
-        Inventory
-        {isInventorySubMenuOpen && (
-          <SubMenu>
-            <div className="inventory-sub-products">Products</div>
-            <div className="inventory-sub-purchase-orders">Purchase Orders</div>
-            <div className="inventory-sub-returns">Returns</div>
-          </SubMenu>
-        )}
-      </Menu>
-      <Menu
-        className="settings-menu with-submenu"
-        onClick={handleSettingsSubMenuToggle}
-      >
-        Settings
-        {isSettingsSubMenuOpen && (
-          <SubMenu>
-            <div className="settings-sub-users">Users</div>
-          </SubMenu>
-        )}
-      </Menu>
-    </SidebarContainer>
-  );
+	return (
+		<SidebarContainer className="sidebar">
+			<h1>SOAPIFY</h1>
+			<Menu className="home-menu">Home</Menu>
+			<Menu className="pos-with with-submenu" onClick={handlePosSubMenuToggle}>
+				POS
+				{isPosSubMenuOpen && (
+					<SubMenu>
+						<div className="pos-sub-sales">Sales</div>
+						<Link href="/dashboard"> Test</Link>
+					</SubMenu>
+				)}
+			</Menu>
+			<Menu className="inventory-menu with-submenu" onClick={handleInventorySubMenuToggle}>
+				Inventory
+				{isInventorySubMenuOpen && (
+					<SubMenu>
+						<div className="inventory-sub-products">Products</div>
+						<div className="inventory-sub-purchase-orders">Purchase Orders</div>
+						<div className="inventory-sub-returns">Returns</div>
+					</SubMenu>
+				)}
+			</Menu>
+			<Menu className="settings-menu with-submenu" onClick={handleSettingsSubMenuToggle}>
+				Settings
+				{isSettingsSubMenuOpen && (
+					<SubMenu>
+						<div className="settings-sub-users">Users</div>
+					</SubMenu>
+				)}
+			</Menu>
+			<Link href="/dashboard/products"> Test</Link>
+		</SidebarContainer>
+	);
 };
 
 export default Sidebar;
---
+++ src/pages/dashboard/products.jsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/components/DashboardLayout";
+
+let ProductsPage = () => {
+	return (
+		<DashboardLayout>
+			<h1>Hello</h1>
+		</DashboardLayout>
+	);
+};
+
+export default ProductsPage;

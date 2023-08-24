55aaa2bc28b912e98bacfff9b7ec5824d71964a2
Angelo <angeloeboy10@gmail.com>
2023-08-24 13:45:53 +0800

    Edited next.config

--- next.config.js
+++ next.config.js
@@ -2,20 +2,19 @@
 const nextConfig = {
 	output: "standalone",
 	async rewrites() {
-		// return [
-		// 	{
-		// 		source: "/api/:path*",
-		// 		destination: "https://iamangelo.tech/api/:path*",
-		// 	},
-		// ];
 		return [
 			{
 				source: "/api/:path*",
-				destination: "http://localhost:3001/:path*",
+				destination: "https://iamangelo.tech/api/:path*",
 			},
 		];
+		// return [
+		// 	{
+		// 		source: "/api/:path*",
+		// 		destination: "http://localhost:3001/:path*",
+		// 	},
+		// ];
 	},
-
 };
 
 module.exports = nextConfig;

9b36ff9167eba70fa820998ba8388ed462ec25e6
NicolasOrtega <janorteganicolas123@gmail.com>
2023-09-07 10:37:37 +0800

    added the user table and added the modal of addUser

--- src/pages/dashboard/user/index.jsx
+++ src/pages/dashboard/user/index.jsx
@@ -1,126 +1,68 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 import DashboardLayout from "@/components/misc/dashboardLayout";
-import ItemActionModal, {
-	Button,
-	Select,
-	LabelContainer,
-	Label,
-	Option,
-	FieldContainer,
-	ProfilePictureContainer,
-	FileInput,
-	Centered,
-	SecondaryButton,
-	CloseButton,
-	ButtonsContainer,
-	PopupOverlay,
-	PopupContent,
-	HeaderTitle,
-	FieldTitleLabel,
-	InputHolder,
-} from "@/styled-components/ItemActionModal";
 import StyledPanel from "@/styled-components/StyledPanel";
+import PageTitle from "@/components/misc/pageTitle";
+import Table, {
+  ActionContainer,
+  TableData,
+  TableHeadings,
+  TableRows,
+} from "@/styled-components/TableComponent";
+import { Button } from "@/styled-components/ItemActionModal";
+import TopBar from "@/components/misc/topbar";
+import SearchBarUserComponent from "@/components/user/SearchBar";
+import PopupContentUser from "@/components/user/addUser";
 
-const Popup = ({ onClose, onButtonClick, fileInput }) => {
-	const [fileUploaded, setFileUploaded] = useState(false);
+const User = () => {
+  const [searchQuery, setSearchQuery] = useState(""); // Initialize searchQuery
+  const [isPopupOpen, setPopupOpen] = useState(false);
 
-	const handleFileUpload = () => {
-		setFileUploaded(true);
-	};
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+    // You can perform any other actions related to search here
+  };
 
-	const resetFileUpload = () => {
-		setFileUploaded(false);
-	};
+  const handleOpenPopup = () => {
+    setPopupOpen(true);
+  };
 
-	return (
-		<PopupOverlay>
-			<PopupContent>
-				<HeaderTitle>Add User</HeaderTitle>
-				<FieldContainer>
-					<LabelContainer first>
-						<Label>General Information</Label>{" "}
-					</LabelContainer>
-					<div>
-						<FieldTitleLabel> First Name </FieldTitleLabel>
-						<InputHolder type="text" placeholder="Enter your First Name" />
-					</div>
-					<div>
-						<FieldTitleLabel notFirst>Last Name</FieldTitleLabel>
-						<InputHolder type="text" placeholder="Enter your Last Name" />
-					</div>
-					<div>
-						<FieldTitleLabel notFirst>Username</FieldTitleLabel>
-						<InputHolder type="text" placeholder="Enter your Username" />
-					</div>
-					<div>
-						<FieldTitleLabel notFirst>Image (optional)</FieldTitleLabel>
-						<ProfilePictureContainer>
-							<Centered>
-								<SecondaryButton onClick={onButtonClick}>
-									{fileUploaded ? "You've uploaded a file" : "Click to Upload or Drag and drop an Image"}
-								</SecondaryButton>
-								<FileInput ref={fileInput} onChange={handleFileUpload} />
-							</Centered>
-						</ProfilePictureContainer>
-					</div>
-					<LabelContainer>
-						<Label>Password</Label>
-					</LabelContainer>
-					<div>
-						<FieldTitleLabel notFirst>Password</FieldTitleLabel>
-						<InputHolder type="password" placeholder="Enter your Password" />
-					</div>
-					<div>
-						<FieldTitleLabel notFirst>Confirm Password</FieldTitleLabel>
-						<InputHolder type="password" placeholder="" />
-					</div>
-					<LabelContainer>
-						<Label>Account Type</Label>
-					</LabelContainer>
-					<div>
-						<FieldTitleLabel notFirst>Account Type</FieldTitleLabel>
-						<Select>
-							<Option value="Store Clerk">Store Clerk</Option>
-							<Option value="Store Owner">Store Owner</Option>
-							<Option value="Cashier">Cashier</Option>
-						</Select>
-					</div>
-				</FieldContainer>
+  const handleClosePopup = () => {
+    setPopupOpen(false);
+  };
 
-				<ButtonsContainer>
-					<CloseButton onClick={onClose}>Close</CloseButton>
-					<Button onClick={() => router.push("/dashboard")}>Save</Button>
-				</ButtonsContainer>
-			</PopupContent>
-		</PopupOverlay>
-	);
+  useEffect(() => {
+    // Fetch your users or any other data here using a similar pattern as in the Products component.
+    // For example:
+    // fetchUsers().then((res) => {
+    //   setUsers(res.users || []);
+    // });
+  }, []);
+
+  return (
+    <DashboardLayout>
+      <PageTitle title="Accounts Lists" />
+      <StyledPanel>
+        <SearchBarUserComponent
+          searchQuery={searchQuery}
+          handleSearchChange={handleSearchChange}
+          handleOpenPopup={handleOpenPopup} // Pass handleOpenPopup function
+        />
+        <Table>
+          <tbody>
+            <TableRows heading>
+              <TableHeadings>Name</TableHeadings>
+              <TableHeadings>Username</TableHeadings>
+              <TableHeadings>Status</TableHeadings>
+              <TableHeadings>Type</TableHeadings>
+              <TableHeadings>Actions</TableHeadings>
+            </TableRows>
+          </tbody>
+        </Table>
+      </StyledPanel>
+      {isPopupOpen && <PopupContentUser onClose={handleClosePopup} />}
+    </DashboardLayout>
+  );
 };
 
-const Test = () => {
-	const [isPopupOpen, setPopupOpen] = useState(false);
-	const fileInput = useRef(null);
-
-	const handleOpenPopup = () => {
-		setPopupOpen(true);
-	};
-
-	const handleClosePopup = () => {
-		setPopupOpen(false);
-	};
-
-	const onButtonClick = () => {
-		fileInput.current.click();
-	};
-
-	return (
-		<DashboardLayout>
-			<StyledPanel>
-				<Button onClick={handleOpenPopup}>Create User</Button>
-				{isPopupOpen && <Popup onClose={handleClosePopup} onButtonClick={onButtonClick} fileInput={fileInput} />}
-			</StyledPanel>
-		</DashboardLayout>
-	);
-};
-
-export default Test;
+export default User;

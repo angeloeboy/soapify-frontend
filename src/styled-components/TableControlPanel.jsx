fba9bd25eba74fb2d695bdec9e26fb22cf2a90dc
Angelo <angeloeboy10@gmail.com>
2023-09-08 21:59:30 +0800

    Search for inventory

--- src/styled-components/TableControlPanel.jsx
+++ src/styled-components/TableControlPanel.jsx
@@ -65,6 +65,8 @@ export const DropdownMenu = styled.div`
 	width: 100%;
 	border: 1px solid #e0e0e0;
 	box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+	max-height: 150px;
+	overflow-y: auto;
 `;
 
 export const DropdownItem = styled.div`

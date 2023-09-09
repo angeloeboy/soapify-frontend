001a9205a79105b05d216951767b10db9ab183ed
NicolasOrtega <janorteganicolas123@gmail.com>
2023-09-09 19:23:48 +0800

    added the table of user and its data(static)

--- src/pages/dashboard/user/index.jsx
+++ src/pages/dashboard/user/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
+import Image from "next/image";  
 import DashboardLayout from "@/components/misc/dashboardLayout";
 import StyledPanel from "@/styled-components/StyledPanel";
 import PageTitle from "@/components/misc/pageTitle";
@@ -13,10 +14,15 @@ import { Button } from "@/styled-components/ItemActionModal";
 import TopBar from "@/components/misc/topbar";
 import UserSearchBarComponent from "@/components/misc/userSearchBarAndFilters";
 import PopupContentUser from "@/components/user/addUser";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis, faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+
 
 const User = () => {
   const [searchQuery, setSearchQuery] = useState(""); // Initialize searchQuery
   const [isPopupOpen, setPopupOpen] = useState(false);
+  const [activeActionContainer, setActiveActionContainer] = useState(-1);
+
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
@@ -31,24 +37,45 @@ const User = () => {
     setPopupOpen(false);
   };
 
-  useEffect(() => {
-    // Fetch your users or any other data here using a similar pattern as in the Products component.
-    // For example:
-    // fetchUsers().then((res) => {
-    //   setUsers(res.users || []);
-    // });
-  }, []);
+  // useEffect(() => {
+  //   // Fetch your users or any other data here using a similar pattern as in the Products component.
+  //   // For example:
+  //   // fetchUsers().then((res) => {
+  //   //   setUsers(res.users || []);
+  //   // });
+  // }, []);
+  const userData = [
+    {
+      name: "User 1",
+      username: "user1",
+      status: "Active",
+      type: "Admin",
+    },
+    {
+      name: "User 2",
+      username: "user2",
+      status: "Inactive",
+      type: "User",
+    },
+    {
+      name: "User 3",
+      username: "user3",
+      status: "Active",
+      type: "User",
+    },
+
+  ];
 
   return (
     <DashboardLayout>
       <PageTitle title="Accounts Lists" />
       <StyledPanel>
-        <UserSearchBarComponent
+        {/* <UserSearchBarComponent
           searchQuery={searchQuery}
           handleSearchChange={handleSearchChange}
           handleOpenPopup={handleOpenPopup} // Pass handleOpenPopup function
-        />
-        <Table>
+        /> */}
+      <Table>
           <tbody>
             <TableRows heading>
               <TableHeadings>Name</TableHeadings>
@@ -57,8 +84,41 @@ const User = () => {
               <TableHeadings>Type</TableHeadings>
               <TableHeadings>Actions</TableHeadings>
             </TableRows>
+
+            {userData.map((user, index) => (
+              <TableRows key={index}>
+                  <TableData bold withImage>
+ 									<Image src="/product_img2.png" width={40} height={40} alt={"Product image"} />
+
+									{user.name}
+								</TableData>
+                 <TableData>{user.username}</TableData>
+                <TableData>{user.status}</TableData>
+                <TableData>{user.type}</TableData>
+                <TableData>
+                <FontAwesomeIcon
+										className="ellipsis"
+										icon={faEllipsis}
+										onClick={() => (activeActionContainer === index ? setActiveActionContainer(-1) : setActiveActionContainer(index))}
+									/>
+
+									{activeActionContainer === index && (
+										<ActionContainer onClick={() => setActiveActionContainer(-1)}>
+											<p>
+												{" "}
+												<FontAwesomeIcon icon={faPen} />
+												Edit
+											</p>
+											<p>
+												<FontAwesomeIcon icon={faTrash} /> Delete
+											</p>
+										</ActionContainer> )}
+                </TableData>
+              </TableRows>
+            ))}
           </tbody>
         </Table>
+
       </StyledPanel>
       {isPopupOpen && <PopupContentUser onClose={handleClosePopup} />}
     </DashboardLayout>

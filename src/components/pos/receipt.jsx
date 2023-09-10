5256e44d9f8c6031820c791856f992f44fbc2ffa
ramesestan <rameses.tan.21@gmail.com>
2023-09-10 17:22:54 +0800

    added User submenu under Settings menu. added editUser component in ellipsis

--- src/components/pos/receipt.jsx
+++ src/components/pos/receipt.jsx
@@ -1,12 +1,20 @@
 import { ComponentTitle } from "@/styled-components/pos";
 import Button from "../misc/button";
+import { getTransaction } from "@/api/transaction";
 
 const Receipt = (props) => {
+	const fetchTransaction = async () => {
+		const response = await getTransaction(15);
+		console.log(response);
+	};
+
 	return (
 		<>
 			<ComponentTitle>Receipt</ComponentTitle>
 
-			<Button width={"100%"}>See Receipt</Button>
+			<Button width={"100%"} onClick={() => fetchTransaction()}>
+				See Receipt
+			</Button>
 			<Button width={"100%"}>Print Receipt</Button>
 		</>
 	);

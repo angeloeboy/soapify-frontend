35c22044058cab148de7180f4255208ff3ab2ba8
Angelo <angeloeboy10@gmail.com>
2023-09-14 11:08:17 +0800

    Adding of supplier. On going. Has bug: Adding supplier subsequently doesnt refresh the list of supplier, adding only one works

--- src/api/supplier.js
+++ src/api/supplier.js
@@ -14,3 +14,21 @@ export const getSuppliers = async () => {
 		console.log(error);
 	}
 };
+
+export const addSupplier = async (supplier) => {
+	try {
+		const response = await fetch("/api/supplier", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(supplier),
+			credentials: "include",
+		});
+
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
--- src/pages/dashboard/suppliers/index.jsx
+++ src/pages/dashboard/suppliers/index.jsx
@@ -1,27 +1,25 @@
 import React, { useState, useRef, useEffect } from "react";
-import styled from "styled-components";
 import Image from "next/image";
 import DashboardLayout from "@/components/misc/dashboardLayout";
 import StyledPanel from "@/styled-components/StyledPanel";
 import PageTitle from "@/components/misc/pageTitle";
 import Table, { ActionContainer, TableData, TableHeadings, TableRows } from "@/styled-components/TableComponent";
-import { Button } from "@/styled-components/ItemActionModal";
-import TopBar from "@/components/misc/topbar";
-import UserSearchBarComponent from "@/components/misc/userSearchBarAndFilters";
-import PopupContentUser from "@/components/user/addUser";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsis, faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
-import PopupContentWarehouse from "@/components/warehouse/addWarehouse";
 import WarehouseSearchBarComponent from "@/components/warehouse/SearchBarAndFilter";
 import { getSuppliers } from "@/api/supplier";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import AddSupplierComponent from "@/components/suppliers/addSupplier";
+import SupplierSearchBarComponent from "@/components/suppliers/searchBarAndFilter";
 
 const Suppliers = () => {
 	const [searchQuery, setSearchQuery] = useState(""); // Initialize searchQuery
 	const [isPopupOpen, setPopupOpen] = useState(false);
 	const [activeActionContainer, setActiveActionContainer] = useState(-1);
 	const [suppliers, setSuppliers] = useState([]);
+	const [suppliersDisplay, setSuppliersDisplay] = useState([]); // Initialize suppliersDisplay
 	const [supplierLoading, setSupplierLoading] = useState([]);
 	const handleSearchChange = (event) => {
 		setSearchQuery(event.target.value);
@@ -37,43 +35,35 @@ const Suppliers = () => {
 	};
 
 	useEffect(() => {
+		fetchSuppliers()
+	}, []);
+
+	const fetchSuppliers = () => {
 		setSupplierLoading(true);
 		getSuppliers().then((res) => {
 			console.log(res);
 			setSuppliers(res.suppliers || []);
+			setSuppliersDisplay(res.suppliers || []);
 			setSupplierLoading(false);
 		});
-	}, []);
-
-	const warehouseData = [
-		{
-			warehouse_id: "1",
-			warehouse_name: "warehouseName1",
-			location: "Address123",
-		},
-		{
-			warehouse_id: "2",
-			warehouse_name: "warehouseName2",
-			location: "Address1234",
-		},
-		{
-			warehouse_id: "3",
-			warehouse_name: "warehouseName3",
-			location: "Address12345",
-		},
-	];
+	};
 
 	return (
 		<DashboardLayout>
 			<PageTitle title="Suppliers" />
 			<StyledPanel>
-				<WarehouseSearchBarComponent searchQuery={searchQuery} handleSearchChange={handleSearchChange} handleOpenPopup={handleOpenPopup} />
+				<SupplierSearchBarComponent
+					searchQuery={searchQuery}
+					handleSearchChange={handleSearchChange}
+					handleOpenPopup={handleOpenPopup}
+					suppliers={suppliers}
+					setSuppliersDisplay={setSuppliersDisplay}
+				/>
 
 				<Table>
 					<tbody>
 						<TableRows heading>
 							<TableHeadings> ID</TableHeadings>
-
 							<TableHeadings>Supplier Name</TableHeadings>
 							<TableHeadings>Contact</TableHeadings>
 							<TableHeadings>Address</TableHeadings>
@@ -100,7 +90,7 @@ const Suppliers = () => {
 										</TableData>
 									</TableRows>
 							  ))
-							: suppliers.map((supplier, index) => (
+							: suppliersDisplay.map((supplier, index) => (
 									<TableRows key={supplier.supplier_id}>
 										<TableData>{supplier.supplier_id}</TableData>
 										<TableData>{supplier.supplier_name}</TableData>
@@ -135,7 +125,7 @@ const Suppliers = () => {
 					</tbody>
 				</Table>
 			</StyledPanel>
-			{isPopupOpen && <PopupContentWarehouse onClose={handleClosePopup} />}
+			{isPopupOpen && <AddSupplierComponent onClose={handleClosePopup} fetchSuppliers={fetchSuppliers}/>}
 		</DashboardLayout>
 	);
 };

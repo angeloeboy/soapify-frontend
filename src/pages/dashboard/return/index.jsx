67d6d10835dfbe4edbef4354b52393a54f00a3c6
NicolasOrtega <janorteganicolas123@gmail.com>
2023-10-15 14:39:40 +0800

    creation of return page

--- src/pages/dashboard/return/index.jsx
+++ src/pages/dashboard/return/index.jsx
@@ -5,8 +5,8 @@ import Table, { ActionContainer, TableData, TableHeadings, TableRows } from "@/s
 import StyledPanel from "@/styled-components/StyledPanel";
 import { faEllipsis, faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import AddReturnComponent from "@/components/return/AddReturn";
-import ReturnSearchBar from "@/components/return/SearchBarAndFilter";
+import AddReturnComponent from "@/components/return/addReturn";
+import ReturnSearchBar from "@/components/return/searchBar";
 
 const ReturnPage = () => {
   const [returns, setReturns] = useState([]);

2e276d62b10c7376410ec9f516b334d8263c43ee
Angel <angeloeboy10@gmail.com>
2023-10-21 12:16:59 +0800

    env variables test

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 // const link = process.env.NEXT_PUBLIC_API_LINK || "https://iamangelo.tech/api";
-const link = process.env.NEXT_PUBLIC_API_LINK ? process.env.NEXT_PUBLIC_API_LINK : "https://iamangelo.tech/api";
+// const link = process.env.NEXT_PUBLIC_API_LINK ? process.env.NEXT_PUBLIC_API_LINK : "https://iamangelo.tech/api";
 
 const nextConfig = {
 	output: "standalone",
 	async rewrites() {
-		// return [
-		// 	{
-		// 		source: "/api/:path*",
-		// 		destination: "https://iamangelo.tech/api/:path*",
-		// 	},
-		// ];
 		return [
 			{
 				source: "/api/:path*",
-				destination: "http://localhost:3001/:path*",
+				destination: "https://iamangelo.tech/api/:path*",
 			},
 		];
+		// return [
+		// 	{
+		// 		source: "/api/:path*",
+		// 		destination: "http://localhost:3001/:path*",
+		// 	},
+		// ];
 	},
 };
 

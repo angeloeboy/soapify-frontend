5bb3a86d31df5ac06c3955daddccda2936c24fcd
Angel <angeloeboy10@gmail.com>
2023-10-21 20:18:29 +0800

    Edited config

--- next.config.js
+++ next.config.js
@@ -5,18 +5,18 @@ const link = process.env.NEXT_PUBLIC_API_LINK ? process.env.NEXT_PUBLIC_API_LINK
 const nextConfig = {
 	output: "standalone",
 	async rewrites() {
-		return [
-			{
-				source: "/api/:path*",
-				destination: "https://iamangelo.tech/api/:path*",
-			},
-		];
 		// return [
 		// 	{
 		// 		source: "/api/:path*",
-		// 		destination: `${link}/:path*`,
+		// 		destination: "https://iamangelo.tech/api/:path*",
 		// 	},
 		// ];
+		return [
+			{
+				source: "/api/:path*",
+				destination: `${link}/:path*`,
+			},
+		];
 	},
 };
 

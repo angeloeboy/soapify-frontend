7c450d8923e9b2bc5c11d8ac8294f967f3e0ed3d
ramesestan <rameses.tan.21@gmail.com>
2023-10-29 10:36:35 +0800

    added units sold and top products with option to select start and end date.

--- next.config.js
+++ next.config.js
@@ -1,23 +1,25 @@
 /** @type {import('next').NextConfig} */
 // const link = process.env.NEXT_PUBLIC_API_LINK || "https://iamangelo.tech/api";
-const link = process.env.NEXT_PUBLIC_API_LINK ? process.env.NEXT_PUBLIC_API_LINK : "https://iamangelo.tech/api";
+const link = process.env.NEXT_PUBLIC_API_LINK
+  ? process.env.NEXT_PUBLIC_API_LINK
+  : "https://iamangelo.tech/api";
 
 const nextConfig = {
-	output: "standalone",
-	async rewrites() {
-		// return [
-		// 	{
-		// 		source: "/api/:path*",
-		// 		destination: "https://iamangelo.tech/api/:path*",
-		// 	},
-		// ];
-		return [
-			{
-				source: "/api/:path*",
-				destination: `${link}/:path*`,
-			},
-		];
-	},
+  output: "standalone",
+  async rewrites() {
+    // return [
+    // 	{
+    // 		source: "/api/:path*",
+    // 		destination: "https://iamangelo.tech/api/:path*",
+    // 	},
+    // ];
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${link}/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

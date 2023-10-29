0dd618ba8e68c2358884d07b709e96b4c9cb0ce8
Angel <angeloeboy10@gmail.com>
2023-10-29 15:44:51 +0800

    implemented status of orders

--- src/pages/user/orders.jsx
+++ src/pages/user/orders.jsx
@@ -8,6 +8,39 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import AddReturnComponent from "@/components/return/addReturn";
 import ReturnSearchBar from "@/components/return/searchBar";
 import { getCustomerTransaction } from "@/api/transaction";
+import styled from "styled-components";
+
+const Circle = styled.span`
+	width: 10px;
+	height: 10px;
+	border-radius: 50%;
+	background-color: ${({ status }) => (status === "PENDING" ? "#FFC107" : status === "COMPLETED" ? "#4CAF50" : "#F44336")};
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+	margin-right: 6px;
+`;
+
+const Animated = styled.div`
+	@keyframes fadeInFadeOut {
+		0% {
+			opacity: 0.5;
+		}
+		45% {
+			opacity: 1;
+		}
+		100% {
+			opacity: 0.5;
+		}
+	}
+
+	//add animation
+	animation: fadeInFadeOut 1s ease-in-out infinite;
+	display: block;
+	padding: 0.5rem 1rem;
+
+	/* background-color: ${({ status }) => (status === "PENDING" ? "#FFC107" : status === "COMPLETED" ? "#4CAF50" : "#F44336")}; */
+`;
 
 const Orders = () => {
 	const [returns, setReturns] = useState([]);
@@ -68,7 +101,12 @@ const Orders = () => {
 								<TableData>{transaction.transaction_number}</TableData>
 								<TableData>{transaction.items.length}</TableData>
 								<TableData>{transaction.payment_method.name}</TableData>
-								<TableData>{transaction.status}</TableData>
+								<TableData>
+									<Animated status={transaction.status}>
+										<Circle status={transaction.status} />
+										{transaction.status}
+									</Animated>
+								</TableData>
 
 								<TableData>
 									<FontAwesomeIcon

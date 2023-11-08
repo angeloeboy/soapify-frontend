af0d7d48b9a9d2f9ced3977397e7c64f5bbbac4d
Angel <angeloeboy10@gmail.com>
2023-11-08 09:28:17 +0800

    Fixed categories page

--- src/pages/dashboard/products/categories.jsx
+++ src/pages/dashboard/products/categories.jsx
@@ -3,13 +3,7 @@ import DashboardLayout from "@/components/misc/dashboardLayout";
 
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPen,
-  faTrash,
-  faEllipsis,
-  faFilter,
-  faPlus,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPen, faTrash, faEllipsis, faFilter, faPlus } from "@fortawesome/free-solid-svg-icons";
 import PageTitle from "@/components/misc/pageTitle";
 import TableControlPanel from "@/styled-components/TableControlPanel";
 import StyledPanel from "@/styled-components/StyledPanel";
@@ -19,13 +13,7 @@ import { addProduct, getProductCategories, getProducts } from "@/api/products";
 import { useRouter } from "next/router";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import Table, {
-  ActionContainer,
-  TableData,
-  TableHeadings,
-  TableRows,
-  Status,
-} from "@/styled-components/TableComponent";
+import Table, { ActionContainer, TableData, TableHeadings, TableRows, Status } from "@/styled-components/TableComponent";
 import { Button } from "@/styled-components/ItemActionModal";
 
 import CategoriesSearchBar from "@/components/product/categories/categoriesSearchBar";
@@ -33,154 +21,128 @@ import AddCategories from "./../../../components/product/categories/addCategorie
 import EditCategory from "@/components/product/categories/editCategory";
 import Pagination from "@/components/misc/pagination";
 const Categories = () => {
-  const [categories, setCategories] = useState([]);
-  const [categoriesDisplay, setCategoriesDisplay] = useState([]);
-  const [categoriesLoading, setCategoriesLoading] = useState(true);
-  const [isAddPopUpOpen, setIsAddPopUpOpen] = useState(false);
-  const [isEditCategoryOpen, setEditCategoryOpen] = useState(false);
-  const [activeActionContainer, setActiveActionContainer] = useState(-1);
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 1;
+	const [categories, setCategories] = useState([]);
+	const [categoriesDisplay, setCategoriesDisplay] = useState([]);
+	const [categoriesLoading, setCategoriesLoading] = useState(true);
+	const [isAddPopUpOpen, setIsAddPopUpOpen] = useState(false);
+	const [isEditCategoryOpen, setEditCategoryOpen] = useState(false);
+	const [activeActionContainer, setActiveActionContainer] = useState(-1);
+	const [currentPage, setCurrentPage] = useState(1);
+	const itemsPerPage = 10;
 
-  useEffect(() => {
-    fetchCategories();
-  }, [currentPage]);
+	const startIndex = (currentPage - 1) * itemsPerPage;
+	const endIndex = currentPage * itemsPerPage;
+	const paginatedCategories = categoriesDisplay.slice(startIndex, endIndex);
 
-  const fetchCategories = () => {
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = currentPage * itemsPerPage;
-    getProductCategories().then((res) => {
-      console.log(res);
-      res.categories
-        ? setCategories(res.categories)
-        : setCategories([]);
-      res.categories
-        ? setCategoriesDisplay(res.categories.slice(startIndex, endIndex))
-        : setCategoriesDisplay([]);
-      setCategoriesLoading(false);
-    });
-  };
+	useEffect(() => {
+		fetchCategories();
+	}, [currentPage]);
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
+	const fetchCategories = () => {
+		getProductCategories().then((res) => {
+			console.log(res);
+			res.categories ? setCategories(res.categories) : setCategories([]);
+			res.categories ? setCategoriesDisplay(res.categories) : setCategoriesDisplay([]);
+			setCategoriesLoading(false);
+		});
+	};
 
-  const openEditPopUp = (category_id) => {
-    setEditCategoryOpen(true);
-  };
+	const handlePageChange = (page) => {
+		setCurrentPage(page);
+	};
 
-  const closeEditPopUp = () => {
-    setEditCategoryOpen(false);
-  };
+	const openEditPopUp = (category_id) => {
+		setEditCategoryOpen(true);
+	};
 
-  
-  const [selectedProductId, setSelectedProductId] = useState(null);
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  return (
-    <DashboardLayout>
-      <PageTitle title="Category List" />
+	const closeEditPopUp = () => {
+		setEditCategoryOpen(false);
+	};
 
-      <StyledPanel>
-        <CategoriesSearchBar
-          setIsAddPopUpOpen={setIsAddPopUpOpen}
-          setCategoriesDisplay={setCategoriesDisplay}
-          categories={categories}
-        />
+	const [selectedProductId, setSelectedProductId] = useState(null);
+	const [selectedCategory, setSelectedCategory] = useState(null);
+	return (
+		<DashboardLayout>
+			<PageTitle title="Category List" />
 
-        <Table>
-          <tbody>
-            <TableRows $heading>
-              <TableHeadings>Name</TableHeadings>
-              <TableHeadings>Status</TableHeadings>
-              <TableHeadings>Actions</TableHeadings>
-            </TableRows>
+			<StyledPanel>
+				<CategoriesSearchBar setIsAddPopUpOpen={setIsAddPopUpOpen} setCategoriesDisplay={setCategoriesDisplay} categories={categories} />
 
-            {categories.length === 0 ? (
-              categoriesLoading ? (
-                Array.from({ length: 8 }, (_, index) => (
-                  <TableRows key={index}>
-                    <TableData className="imgContainer">
-                      <Skeleton circle={true} height={40} width={40} />
-                    </TableData>
+				<Table>
+					<tbody>
+						<TableRows $heading>
+							<TableHeadings>Name</TableHeadings>
+							<TableHeadings>Status</TableHeadings>
+							<TableHeadings>Actions</TableHeadings>
+						</TableRows>
 
-                    <TableData>
-                      <Skeleton width={50} height={20} />
-                    </TableData>
-                    <TableData>
-                      <Skeleton width={50} height={20} />
-                    </TableData>
-                  </TableRows>
-                ))
-              ) : (
-                <p>No Categories found</p>
-              )
-            ) : (
-              categoriesDisplay.map((category, index) => (
-                <TableRows key={category.category_id}>
-                  <TableData>{category.name}</TableData>
+						{categories.length === 0 ? (
+							categoriesLoading ? (
+								Array.from({ length: 8 }, (_, index) => (
+									<TableRows key={index}>
+										<TableData className="imgContainer">
+											<Skeleton circle={true} height={40} width={40} />
+										</TableData>
 
-                  <TableData>
-                    {category.isActive ? "Active" : "Not active"}
-                  </TableData>
+										<TableData>
+											<Skeleton width={50} height={20} />
+										</TableData>
+										<TableData>
+											<Skeleton width={50} height={20} />
+										</TableData>
+									</TableRows>
+								))
+							) : (
+								<p>No Categories found</p>
+							)
+						) : (
+							paginatedCategories.map((category, index) => (
+								<TableRows key={category.category_id}>
+									<TableData>{category.name}</TableData>
 
-                  <TableData>
-                    <FontAwesomeIcon
-                      className="ellipsis"
-                      icon={faEllipsis}
-                      onClick={() =>
-                        activeActionContainer === index
-                          ? setActiveActionContainer(-1)
-                          : setActiveActionContainer(index)
-                      }
-                    />
+									<TableData>{category.isActive ? "Active" : "Not active"}</TableData>
 
-                    {activeActionContainer === index && (
-                      <ActionContainer
-                        onClick={() => setActiveActionContainer(-1)}
-                      >
-                        <p
-                          onClick={() => {
-                            setSelectedCategory();
-                            openEditPopUp(selectedCategory);
-                          }}
-                        >
-                          <FontAwesomeIcon icon={faPen} />
-                          Edit
-                        </p>
-                        <p>
-                          <FontAwesomeIcon icon={faTrash} /> Delete
-                        </p>
-                      </ActionContainer>
-                    )}
-                  </TableData>
-                </TableRows>
-              ))
-            )}
-          </tbody>
-        </Table>
-      </StyledPanel>
-      {isAddPopUpOpen && (
-        <AddCategories
-          setIsAddPopUpOpen={setIsAddPopUpOpen}
-          fetchCategories={fetchCategories}
-        />
-      )}
-      {isEditCategoryOpen && (
-        <EditCategory
-          onClose={closeEditPopUp}
-          setEditCategoryOpen={setEditCategoryOpen}
-        />
-      )}
+									<TableData>
+										<FontAwesomeIcon
+											className="ellipsis"
+											icon={faEllipsis}
+											onClick={() => (activeActionContainer === index ? setActiveActionContainer(-1) : setActiveActionContainer(index))}
+										/>
 
-      <Pagination
-        totalItems={categoriesDisplay.length} // Total number of items
-        itemsPerPage={itemsPerPage}
-        currentPage={currentPage}
-        onPageChange={(newPage) => setCurrentPage(newPage)}
-      />  
+										{activeActionContainer === index && (
+											<ActionContainer onClick={() => setActiveActionContainer(-1)}>
+												<p
+													onClick={() => {
+														setSelectedCategory();
+														openEditPopUp(selectedCategory);
+													}}
+												>
+													<FontAwesomeIcon icon={faPen} />
+													Edit
+												</p>
+												<p>
+													<FontAwesomeIcon icon={faTrash} /> Delete
+												</p>
+											</ActionContainer>
+										)}
+									</TableData>
+								</TableRows>
+							))
+						)}
+					</tbody>
+				</Table>
+			</StyledPanel>
+			{isAddPopUpOpen && <AddCategories setIsAddPopUpOpen={setIsAddPopUpOpen} fetchCategories={fetchCategories} />}
+			{isEditCategoryOpen && <EditCategory onClose={closeEditPopUp} setEditCategoryOpen={setEditCategoryOpen} />}
 
-    </DashboardLayout>
-  );
+			<Pagination
+				totalItems={categoriesDisplay.length} // Total number of items
+				itemsPerPage={itemsPerPage}
+				currentPage={currentPage}
+				onPageChange={(newPage) => setCurrentPage(newPage)}
+			/>
+		</DashboardLayout>
+	);
 };
 
 export default Categories;

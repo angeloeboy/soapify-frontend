47dc62da37702ac9a724ad72321f49a6a656d651
Angel <angeloeboy10@gmail.com>
2023-11-12 13:35:56 +0800

    Display of sidebar based on permissions

--- src/middleware.js
+++ src/middleware.js
@@ -18,6 +18,69 @@ export async function middleware(req) {
 	}
 
 	try {
+		// if (token) {
+		// 	try {
+		// 		const decoded = jwt.decode(token.value, secretKey);
+		// 		permissions = decoded.permissions || [];
+		// 	} catch (verifyError) {
+		// 		console.error("JWT verification error:", verifyError);
+		// 		const url = req.nextUrl.clone();
+		// 		url.pathname = "/login";
+		// 		return NextResponse.redirect(url);
+		// 	}
+		// }
+
+		// // If accessing dashboard without a token, redirect to home
+		// if (req.nextUrl.pathname.startsWith("/dashboard") && !token) {
+		// 	const url = req.nextUrl.clone();
+		// 	url.pathname = "/";
+		// 	return NextResponse.redirect(url);
+		// }
+
+		// // If accessing root or login with a token, verify and redirect to dashboard if valid
+		// if ((req.nextUrl.pathname === "/" || req.nextUrl.pathname.startsWith("/login")) && token) {
+		// 	const response = await fetch(auth_link + "/verify-token", {
+		// 		headers: {
+		// 			Cookie: `token=${token.value}`,
+		// 		},
+		// 	});
+
+		// 	if (response.ok) {
+		// 		const url = req.nextUrl.clone();
+		// 		url.pathname = "/dashboard";
+		// 		return NextResponse.redirect(url);
+		// 	}
+		// }
+
+		// // For dashboard route, if token is not valid, redirect to login
+		// if (req.nextUrl.pathname.startsWith("/dashboard")) {
+		// 	const response = await fetch(auth_link + "/verify-token", {
+		// 		headers: {
+		// 			Cookie: `token=${token.value}`,
+		// 		},
+		// 	});
+
+		// 	let data = await response.json();
+		// 	if (data.user.role_id !== 1) {
+		// 		const url = req.nextUrl.clone();
+		// 		url.pathname = "/user";
+		// 		return NextResponse.redirect(url);
+		// 	}
+
+		// 	const url = req.nextUrl.clone();
+		// 	url.pathname = "/";
+
+		// 	return response.ok ? NextResponse.next() : NextResponse.redirect(url);
+		// }
+
+		// const next_response = NextResponse.next();
+		// next_response.cookies.set("permissions", JSON.stringify(permissions), {
+		// 	httpOnly: false,
+		// 	path: "/",
+		// });
+
+		// return next_response;
+
 		if (token) {
 			try {
 				const decoded = jwt.decode(token.value, secretKey);
@@ -48,7 +111,14 @@ export async function middleware(req) {
 			if (response.ok) {
 				const url = req.nextUrl.clone();
 				url.pathname = "/dashboard";
-				return NextResponse.redirect(url);
+
+				// Set cookies before redirecting
+				const next_response = NextResponse.redirect(url);
+				next_response.cookies.set("permissions", JSON.stringify(permissions), {
+					httpOnly: false,
+					path: "/",
+				});
+				return next_response;
 			}
 		}
 
@@ -70,9 +140,16 @@ export async function middleware(req) {
 			const url = req.nextUrl.clone();
 			url.pathname = "/";
 
-			return response.ok ? NextResponse.next() : NextResponse.redirect(url);
+			// Set cookies here as well
+			const next_response = response.ok ? NextResponse.next() : NextResponse.redirect(url);
+			next_response.cookies.set("permissions", JSON.stringify(permissions), {
+				httpOnly: false,
+				path: "/",
+			});
+			return next_response;
 		}
 
+		// Set cookies for all other routes
 		const next_response = NextResponse.next();
 		next_response.cookies.set("permissions", JSON.stringify(permissions), {
 			httpOnly: false,
@@ -80,7 +157,6 @@ export async function middleware(req) {
 		});
 
 		return next_response;
-		// return NextResponse.next();
 	} catch (error) {
 		console.error("Error in middleware:", error);
 		const url = req.nextUrl.clone();
